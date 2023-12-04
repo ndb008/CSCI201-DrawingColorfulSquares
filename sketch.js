89e61429759fa48da270a648af90f84ee2da87e1
ndb008 <152889015+ndb008@users.noreply.github.com>
2023-12-04 09:15:27 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var squareCount = 0;
+
+function setup() {
+  createCanvas(400, 400);
+  background(255);
+}
+
+function draw() {
+  let colorValue = map(squareCount, 0, 500, 0, 255);
+  fill(colorValue, 100, 150);
+  stroke(0);
+  strokeWeight(2);
+
+  if (mouseIsPressed) {
+    square(mouseX, mouseY, 40);
+    squareCount++;
+  }
+}
